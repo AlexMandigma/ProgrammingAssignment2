911fc1cecb3fe89ed7c3de6ceda5f162a798357d
Alexander Mandigma <alexander.mandigma@gmail.com>
2017-01-16 23:09:13 +0800

    Assignment
    Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Inverse of a Matrix
+##The functions will look for the inverse matrix of an invertible matrix
 
-## Write a short comment describing this function
+##We will be using the solve function since 
+##Solve-This generic function solves the equation a %*% x = b for x, where b can be either a vector or a matrix.
+##https://stat.ethz.ch/R-manual/R-devel/library/base/html/solve.html 
+##From the assignment https://github.com/rdpeng/ProgrammingAssignment2 , the mean was edited to Solve to get the answer
+
+##This function creates a special "matrix" object that can cache its inverse
+
+#1. set the value of the matrix
+#2. get the value of the matrix
+#3. set the value of the inverse using the Solve function
+#4. get the value of the inverse using the solve function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the mean of the special "vector" created with the above function. However, 
+##it first checks to see if the mean has already been calculated. 
+##If so, it gets the mean from the cache and skips the computation. 
+##Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
